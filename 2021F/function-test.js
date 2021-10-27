1667ce09bb9ad0804199667cf00dea921f3966f8
chisato <m0a2003438@edu.teu.ac.jp>
2021-10-27 18:49:36 +0900

    2021f scriptタグのテスト

--- 2021F/function-test.js
+++ 2021F/function-test.js
@@ -1,4 +1,13 @@
-function Hello(){
-  console.log(`テスト`);
+function Hello(犬1, 犬2, 犬3){
+  console.log(`犬1 ${犬1}`);
+  console.log(`犬2 ${犬2}`);
+  console.log(`犬3 ${犬3}`);
 }
-Hello("テスト");
+
+Hello(1, 2, 3);
+
+function Dog(犬2 = "デフォルト"){
+  console.log(`犬2: ${犬2}`);
+}
+
+console.log("パグ");
