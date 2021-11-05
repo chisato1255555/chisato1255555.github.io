02ebe529580fb316803118e30e97e88d646c8d18
chisato <m0a2003438@edu.teu.ac.jp>
2021-11-06 00:17:45 +0900

    20211103 課題

---
+++ 20211103/Test2.js
@@ -0,0 +1,17 @@
+function  changeRed(){
+  let modal = document
+      .getElementById(`modal`);
+  modal.style.backgroundColor = `red`;
+}
+
+function  changeYellow(){
+  let modal = document
+      .getElementById("modal");
+  modal.style.backgroundColor = `yellow`;
+}
+
+function  changeGreen(){
+  let modal = document
+      .getElementById("modal");
+  modal.style.backgroundColor = `green`;
+}
