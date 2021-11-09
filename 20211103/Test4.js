5739d33c38d429950773cde7d92d3964855f8dab
chisato <m0a2003438@edu.teu.ac.jp>
2021-11-09 21:45:54 +0900

    20211103 課題

---
+++ 20211103/Test4.js
@@ -0,0 +1,13 @@
+let modal = document.querySelector("#modal");
+
+function plusOne() {
+  modal.innerHTML = String(Number(modal.innerHTML) + 1);
+}
+
+function plusFive() {
+  modal.innerHTML = String(Number(modal.innerHTML) + 5);
+}
+
+function plusTen() {
+  modal.innerHTML = String(Number(modal.innerHTML) + 10);
+}
