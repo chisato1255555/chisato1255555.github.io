7457eed50b5147fa8b79681744c3f1738aecb75b
chisato <m0a2003438@edu.teu.ac.jp>
2021-11-16 15:12:24 +0900

    20211103 課題

--- 2021/20211110/kadaiB.js
+++ 2021/20211110/kadaiB.js
@@ -1,11 +1,16 @@
 function checkYear() {
   let num = document.querySelector(`#year`);
   let answer = document.querySelector(`#answer`);
-  if (Number(num.value) %4===0) {
+  if (Number(num.value) % 4 === 0) {
     answer.innerHTML = "閏年";
-  }else if(Number(num.value) %100!==0) {
+  }
+  if (Number(num.value) % 400 === 0) {
+    answer.innerHTML = "閏年";
+  }
+  else if (Number(num.value) % 100 === 0) {
     answer.innerHTML = "平年";
-  }else if(Number(num.value) %400===0) {
+  }
+  if (Number(num.value) % 100 !== 0) {
     answer.innerHTML = "閏年";
   }
 }
