095d423aa30e715a9164cca9ace4f13795f074ad
chisato <m0a2003438@edu.teu.ac.jp>
2021-11-21 14:18:32 +0900

    20211103 課題

--- 2021/1117/kadai2.js
+++ 2021/1117/kadai2.js
@@ -1,24 +1,24 @@
-function  doFizzBuzz(){
+function  doFizzBuzz() {
   let answer = document.querySelector(`#answer`);
-  let sum = 1;
   let i = 1;
-  while(i <= 100){
+  while (i <= 100) {
     console.log(i);
-    if (i % 3){
-      answer.innerHTML = "Fizz<br>";
-      console.log("Fizz");
-    }
-    else if (i % 5){
-      answer.innerHTML = "Buzz<br>";
-      console.log("Buzz");
-    }
-    else if (i % 3 && sum % 5){
+    if (i % 3 === 0 && i % 5 === 0) {
       answer.innerHTML = "FizzBuzz<br>";
       console.log("FizzBuzz");
     }
-    else{
+    else if (i % 3 === 0) {
+      answer.innerHTML = "Fizz<br>";
+      console.log("Fizz");
+    }
+    else if (i % 5 === 0) {
+      answer.innerHTML = "Buzz<br>";
+      console.log("Buzz");
+    }
+    else {
       answer.innerHTML = "i<br>";
     }
+    answer.innerHTML = answer.innerHTML + "<br>";
     i++;
   }
 }
