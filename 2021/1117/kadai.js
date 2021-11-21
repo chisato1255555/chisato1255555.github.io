18fe8b723a59f82d3d81dbc65f715af176a19c22
chisato <m0a2003438@edu.teu.ac.jp>
2021-11-21 22:32:46 +0900

    20211103 課題

--- 2021/1117/kadai.js
+++ 2021/1117/kadai.js
@@ -1,15 +1,15 @@
 function  doCheckAnswer() {
   let answer = document.querySelector("#answer");
   let input = document.querySelector("#input_string");
-  let answerList = document.querySelectorAll("ul.answer-list");
+  let answerList = document.querySelectorAll("ul.answer-list li");
   let answerArray = Array.from(answerList);
 
-    answerArray.forEach(function(answer) {
-      if (answer.innerHTML === input.value) {
+    answerArray.forEach(function(answer1) {
+      if (answer1.innerHTML === input.value) {
         answer.innerHTML = "正解";
       }
       else {
-        answer.innerHTML = "不正解";
+        answer1.innerHTML = "不正解";
       }
     })
 }
