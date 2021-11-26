c604b77d410de1d635d61915b11f6375b2d122c0
chisato <m0a2003438@edu.teu.ac.jp>
2021-11-26 12:48:41 +0900

    20211103 課題

---
+++ 2021/中間課題/Twitter.js
@@ -0,0 +1,11 @@
+let answer = document.querySelector("#name");
+let i = 0;
+
+for(let i = 0;i < tweets.length;i++){
+  answer.innerHTML += `<li>
+    <b>${tweets[i].name}</b>:
+    ${tweets[i].message}
+    <i>${tweets[i].tweetedAt}</i>
+  </li>`;
+}
+answer.innerHTML = `<ul>${answer.innerHTML}</ul>`;
