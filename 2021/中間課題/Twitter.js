be8a3fa8de5d122d0328c18b4c34102117021269
chisato <m0a2003438@edu.teu.ac.jp>
2021-11-28 19:38:18 +0900

    2021 中間課題

--- 2021/中間課題/Twitter.js
+++ 2021/中間課題/Twitter.js
@@ -15,6 +15,7 @@ let tweets = [
 ];
 
 for(let i = 0;i <tweets.length;i++){
+  let answer = document.querySelector("#name");
   answer.innerHTML += `<li>
     <img src=" ${tweets[i].avatar}"
     <b>${tweets[i].name}</b>:
@@ -24,7 +25,9 @@ for(let i = 0;i <tweets.length;i++){
 }
 answer.innerHTML = `<ul>${answer.innerHTML}</ul>`;
 
+
 function all(){
+  let answer = document.querySelector("#name");
   for(let i = 0;i <tweets.length;i++){
   answer.innerHTML += `<li>
     <img src=" ${tweets[i].avatar}"
@@ -33,24 +36,26 @@ function all(){
     <i>${tweets[i].tweetedAt}</i>
   </li>`;
   }
-answer.innerHTML = `<ul>${answer.innerHTML}</ul>`;
+  answer.innerHTML = `<ul>${answer.innerHTML}</ul>`;
 }
 
 function sanbot() {
+  let answer = document.querySelector("#name");
   for (let i = 0; i < tweets.length; i++) {
-    if (tweets[i].name === `三郎BOT`) {
-      answer.innerHTML += `<li>
+      if (tweets[i].name === `三郎BOT`) {
+        answer.innerHTML += `<li>
     <img src=" ${tweets[i].avatar}"
     <b>${tweets[i].name}</b>:
     ${tweets[i].message}
     <i>${tweets[i].tweetedAt}</i>
   </li>`;
+      }
     }
-  }
     answer.innerHTML = `<ul>${answer.innerHTML}</ul>`;
 }
 
 function jirou() {
+  let answer = document.querySelector("#name");
   for (let i = 0; i < tweets.length; i++) {
     if (tweets[i].name === `次郎`) {
       answer.innerHTML += `<li>
@@ -65,6 +70,7 @@ function jirou() {
 }
 
 function tarou() {
+  let answer = document.querySelector("#name");
   for (let i = 0; i < tweets.length; i++) {
     if (tweets[i].name === `太郎`) {
       answer.innerHTML += `<li>
