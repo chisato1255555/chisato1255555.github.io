40732ae7583465de76e1aa76f4d8c88e0eab1bd1
chisato <m0a2003438@edu.teu.ac.jp>
2021-12-14 16:23:26 +0900

    2021 12月8日課題

--- 2021/1208/Kadai.js
+++ 2021/1208/Kadai.js
@@ -1,21 +1,15 @@
 function doFetch() {
-  fetch("https://api.openbd.jp/v1/get?isbn=9784591159224").then(
-      function (response) {
+  const code = document.getElementById('isbn').value;
+  const parent = document.getElementById('bookinfo');
+  parent.textContent = null;
+
+  fetch('https://api.openbd.jp/v1/get?isbn=9784591159224' + code)
+      .then(response =>  {
         return response.json();
-      }
-  ).then(
-      function (json) {
-        console.log(json);
-        let html = "<table>";
-        json.values.forEach((row) => {
-          html += `<tr>
-                  <td>${row[0]}</td>
-                  <td>${row[1]}</td>
-                  <td>${row[2]}</td>
-                </tr>`;
-        });
-        html += "</table>";
-        document.querySelector("#answer").innerHTML = html;
-      }
-  );
+      }).then( res => {
+    const data = "書籍名:" + res.items[0].volumeInfo.title;
+    parent.append(data);
+  }).catch(function (error) {
+    parent.append(error);
+  });
 }
